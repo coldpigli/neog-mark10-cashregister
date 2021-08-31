9db3cc5c64b96cc695536b11328e5d2ab6b174d0
coldpigli <piyushdas98@gmail.com>
2021-08-31 22:31:11 +0530

    functionality now working

--- src/App.js
+++ src/App.js
@@ -1,36 +1,69 @@
+import { set } from 'object-path';
 import React, { useState } from 'react';
 import './index.css';
 
 function App() {
+
+  const[billAmount, setBillAmount] = useState();
+  const[amountPaid, setAmountPaid] = useState();
+  const[alertMessage, setAlertMessage] = useState("");
+  const availableDenominations = [2000, 500, 100, 20, 10, 5, 1];
+  let numberArray = [0,0,0,0,0,0,0];
+  const [ValueInTable,setValueInTable] = useState(numberArray);
+  
+
+
+  const valdatingInputs = () => {
+      if(billAmount>0){
+        //calculate change
+        if(amountPaid<billAmount){
+          setAlertMessage("You seem to short on money. Wanna do the dishes?")
+        }
+        else{
+          setAlertMessage("");
+          calculateDenomination(amountPaid-billAmount);
+        }
+      }
+      else{
+        setAlertMessage("Bill amount cannot be 0 or less!!")
+      }
+  }
+
+  const calculateDenomination = (money) => {
+    for(let i = 0; i<availableDenominations.length; i++){
+      const numberOfNotes = Math.trunc(money/availableDenominations[i]);
+      money = money % availableDenominations[i];
+      numberArray[i] = numberOfNotes;
+      
+    }
+    setValueInTable(numberArray);
+  }
   
   
   return (<div className= "container"> 
     <h1>Cash Register</h1>
     <p>Cash Register is a simple web app that tells you what denominations to return as change</p>
 
-    <label for="billAmount">Enter the bill amount</label>
-    <input id = "billAmount" type = "number"></input>
+    <label htmlFor="billAmount">Enter the bill amount</label>
+    <input id = "billAmount" type = "number"  onChange = {(e)=>{setBillAmount(e.target.value)}}></input>
 
-    <label for="amountPaid">Enter the amount paid</label>
-    <input id = "amountPaid" type = "number"></input>
+    <label htmlFor="amountPaid">Enter the amount paid</label>
+    <input id = "amountPaid" type = "number"  onChange = {(e)=>{setAmountPaid(e.target.value)}}></input>
 
-    <button>Calculate Denominations</button>
-    <h3>Alert Message here</h3>
+    <button onClick = {valdatingInputs}>Calculate</button>
+    <h3>{alertMessage}</h3>
 
 
-    <table class="change-table">
+    <table className="change-table">
           <caption>
             Denominations to be returned
           </caption>
           <tbody><tr>
             <th>No of Notes</th>
-            <td class="no-of-notes"></td>
-            <td class="no-of-notes"></td>
-            <td class="no-of-notes"></td>
-            <td class="no-of-notes"></td>
-            <td class="no-of-notes"></td>
-            <td class="no-of-notes"></td>
-            <td class="no-of-notes"></td>
+            {ValueInTable.map((number)=>{
+              return <td className="no-of-notes">{number}</td>
+            })}
+            
           </tr>
           <tr>
             <th>Note</th>
