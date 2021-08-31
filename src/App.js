a180fb215ffe72af4d1acaa70fa13d660c0dce8f
coldpigli <piyushdas98@gmail.com>
2021-08-31 21:04:23 +0530

    completed UI for the register

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
 import React, { useState } from 'react';
-import data from './data';
+import './index.css';
+
 function App() {
   
   
-  return (
-  <h2>lorem ipsum project setup</h2>
+  return (<div className= "container"> 
+    <h1>Cash Register</h1>
+    <p>Cash Register is a simple web app that tells you what denominations to return as change</p>
+
+    <label for="billAmount">Enter the bill amount</label>
+    <input id = "billAmount" type = "number"></input>
+
+    <label for="amountPaid">Enter the amount paid</label>
+    <input id = "amountPaid" type = "number"></input>
+
+    <button>Calculate Denominations</button>
+    <h3>Alert Message here</h3>
+
+
+    <table class="change-table">
+          <caption>
+            Denominations to be returned
+          </caption>
+          <tbody><tr>
+            <th>No of Notes</th>
+            <td class="no-of-notes"></td>
+            <td class="no-of-notes"></td>
+            <td class="no-of-notes"></td>
+            <td class="no-of-notes"></td>
+            <td class="no-of-notes"></td>
+            <td class="no-of-notes"></td>
+            <td class="no-of-notes"></td>
+          </tr>
+          <tr>
+            <th>Note</th>
+            <td>2000</td>
+            <td>500</td>
+            <td>100</td>
+            <td>20</td>
+            <td>10</td>
+            <td>5</td>
+            <td>1</td>
+          </tr>
+        </tbody></table>
+  </div>
+  
     )
 }
 
